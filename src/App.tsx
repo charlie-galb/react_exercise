9b060c7280f8fc438821c87ad9fd4dea5964000c
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-01 15:56:44 +0100

    Add Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import GlobalStyle from './GlobalStyle';
 import AppHeader from './components/Header/Header'
+import Home from './pages/Home/Home'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <AppHeader>Recipe Manager</AppHeader>
+      <Home />
     </>
   );
 }
