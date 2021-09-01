04a8e8d1525da153a1d5716d96dda766e5f02283
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-01 15:56:57 +0100

    Add Link component

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+interface Props {
+    children: React.ReactNode
+}
+
+const Link: React.FC<Props> = (props: Props) => {
+    const { children } = props
+    return (
+        <LinkText>{children}</LinkText>
+    )
+}
+
+const LinkText = styled.a`
+    color: green;
+`
+
+export default Link
