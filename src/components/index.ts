635f6bf8b04a9c04725a097302d4b7b58f5c5837
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 17:22:48 +0100

    Add index file for components directory

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import Banner from './Banner'
+import Button from './Button'
+import Form from './Form'
+import HeaderText from './HeaderText'
+import Input from './Input'
+import Link from './Link'
+import List from './List'
+import Para from './Para'
+import Section from './Section'
+import SubHeaderText from './SubHeaderText'
+
+export { 
+    Banner,
+    Button,
+    Form,
+    HeaderText,
+    Input,
+    Link,
+    List,
+    Para,
+    Section,
+    SubHeaderText
+}
