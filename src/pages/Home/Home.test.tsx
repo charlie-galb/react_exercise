95baf5793c02709fed3386cc3d9233e7dc414ae7
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 16:10:51 +0100

    Wrap state changes in act to remove warning

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -6,18 +6,21 @@ import { recipeArr } from '../../data/testData'
 import renderWithRouter from '../../utils/testUtils/renderWithRouter'
 
 jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('Home', () => {
-    it('retrieves recipes from backend upon rendering', () => {
-        axios.get.mockResolvedValue({data: recipeArr})
+    it('retrieves recipes from backend upon rendering', async () => {
+        mockedAxios.get.mockResolvedValue({data: recipeArr})
+        await act(() => Promise.resolve())
         render(
             <Home />
         )
-        expect(axios.get).toHaveBeenCalledTimes(1)
+        expect(mockedAxios.get).toHaveBeenCalledTimes(1)
     })
     it('redirects when the "Create a new recipe" button is clicked', async () => {
-        axios.get.mockResolvedValue({data: recipeArr})
+        mockedAxios.get.mockResolvedValue({data: recipeArr})
         const { getByText, history } = renderWithRouter(<Home />, '/')
+        await act(() => Promise.resolve())
         const btn = getByText('Create a new recipe')
         fireEvent.click(btn)
         expect(history.location.pathname).toEqual('/create_recipe')
