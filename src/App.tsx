d9d87ebf40a30d4009337c9a58ee6202e557df3c
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 17:22:55 +0100

    Rename Header to Banner

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import GlobalStyle from './GlobalStyle';
-import AppHeader from './components/Header'
+import Banner from './components/Banner'
 import Home from './pages/Home/Home'
 import CreateRecipe from './pages/CreateRecipe/CreateRecipe'
 import Recipe from './pages/Recipe/Recipe'
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <Router>
       <GlobalStyle />
-      <AppHeader>Recipe Manager</AppHeader>
+      <Banner>Recipe Manager</Banner>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/create_recipe' component={CreateRecipe} />
