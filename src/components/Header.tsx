88b6c6383f77df706b530ab2a7586d6dd34f63ee
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 17:21:40 +0100

    Add text components

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components"
-
-const Header = styled.h1`
-    background-color: red;
-    text-align: center;
-`
-
-export default Header
---
+++ src/components/SubHeaderText.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+const SubHeaderText = styled.h2`
+    text-align: center;
+`
+
+export default SubHeaderText
