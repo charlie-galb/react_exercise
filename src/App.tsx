c7619a52c168c9d65aaa227e09dc3a0755036117
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 11:43:32 +0100

    Add routing for / and create_recipe

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import GlobalStyle from './GlobalStyle';
-import AppHeader from './components/Header/Header'
+import AppHeader from './components/Header'
 import Home from './pages/Home/Home'
+import CreateRecipe from './pages/CreateRecipe/CreateRecipe'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <AppHeader>Recipe Manager</AppHeader>
-      <Home />
-    </>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/Create_recipe' component={CreateRecipe} />
+      </Switch>
+    </Router>
   );
 }
 
