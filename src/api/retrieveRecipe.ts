fc059c05f105364b40f3212f49d5707523fe5858
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-09-02 17:22:26 +0100

    Replace hardocded id with variable

--- src/api/retrieveRecipe.ts
+++ src/api/retrieveRecipe.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Recipe from '../types/Recipe'
 
 const retrieveRecipes = (id: string, callback: (responseData: Recipe) => void) => {
-    axios.get<Recipe>(`/api/recipe/recipes/${1}`)
+    axios.get<Recipe>(`/api/recipe/recipes/${id}`)
         .then(response => {
             callback(response.data)
         })
